96419e05312b7461c76ecb0ab7ebc836218bb65e
bekanay <beki.bekovich2004@gmail.com>
2025-06-24 19:18:46 +0500

    server is running, flags and handlers initialized

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Incorrect usage. Type triple-s --help")
+	}
+	tripleCmd := flag.NewFlagSet("triplet", flag.ExitOnError)
+	portNumber := tripleCmd.String("port", "", "port number on which server is running")
+	isHelp := tripleCmd.Bool("help", false, "usage of triplet-s program")
+	directory := tripleCmd.String("dir", "", "directory of storage")
+	tripleCmd.Parse(os.Args[1:])
+
+	if *isHelp {
+		log.Fatal("usage")
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", route)
+	log.Println("Starting server on :", *portNumber)
+	err := http.ListenAndServe(*portNumber, mux)
+	log.Fatal(err)
+}
+
+func route(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if r.Method == http.MethodPut {
+		if len(path) > 1 {
+			path = path[1:]
+			bucketName := path
+			handleCreateBucket(w, r, bucketName)
+			return
+		}
+		http.Error(w, "Bucket name is required", http.StatusBadRequest)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+	}
+
+	if r.Method == http.MethodDelete {
+	}
+}
+
+func handleCreateBucket(w http.ResponseWriter, r *http.Request, bucketName string) {
+	if len(bucketName) < 3 || len(bucketName) > 63 {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	for i := 0; i < len(bucketName); i++ {
+		if bucketName[i] >= 'a' && bucketName[i] <= 'z' {
+			continue
+		}
+		if bucketName[i] >= '0' && bucketName[i] <= '9' {
+			continue
+		}
+		if bucketName[i] == '-' || bucketName[i] == '.' {
+			continue
+		}
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Success"))
+}
+
+func handleListBuckets(w http.ResponseWriter, r *http.Request) {
+}
+
+func handleDeleteBucket(w http.ResponseWriter, r *http.Request) {
+}
