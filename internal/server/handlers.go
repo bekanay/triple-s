3480daf8e5aa6799b82157f95e952676edd4d9b6
bekanay <beki.bekovich2004@gmail.com>
2025-07-08 17:48:24 +0500

    panic is solved, xml responses added

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/xml"
-	"log"
 	"net/http"
 	"strings"
 	"triple-s/internal/storage"
@@ -10,6 +9,11 @@ import (
 
 type PutObjectResult struct {
 	XMLName xml.Name `xml:"PutObjectResult"`
+	ETag    string   `xml:"ETag"`
+}
+
+type PutBucketResult struct {
+	XMLName xml.Name `xml:"PutBucketResult"`
 	ETag    string   `xml:"ETag"`
 }
 
@@ -29,7 +33,11 @@ func (s *Server) handleBuckets(w http.ResponseWriter, r *http.Request) {
 		case 1:
 			s.uploadObject(w, r, parts[0], parts[1])
 		default:
-			http.Error(w, "can not use more than 2 segments in path", http.StatusBadRequest)
+			resp := ErrorResponse{
+				Code:    "400",
+				Message: "can not use more than 2 segments in path",
+			}
+			writeXML(w, http.StatusBadRequest, resp)
 		}
 
 	case http.MethodGet:
@@ -41,7 +49,11 @@ func (s *Server) handleBuckets(w http.ResponseWriter, r *http.Request) {
 		case 1:
 			s.getObject(w, r, parts[0], parts[1])
 		default:
-			http.Error(w, "can not use more than 2 segments in path", http.StatusBadRequest)
+			resp := ErrorResponse{
+				Code:    "400",
+				Message: "can not use more than 2 segments in path",
+			}
+			writeXML(w, http.StatusBadRequest, resp)
 		}
 
 	case http.MethodDelete:
@@ -57,21 +69,35 @@ func (s *Server) handleBuckets(w http.ResponseWriter, r *http.Request) {
 		case 1:
 			s.deleteObject(w, r, parts[0], parts[1])
 		default:
-			http.Error(w, "can not use more than 2 segments in path", http.StatusBadRequest)
+			resp := ErrorResponse{
+				Code:    "400",
+				Message: "can not use more than 2 segments in path",
+			}
+			writeXML(w, http.StatusBadRequest, resp)
 		}
 
 	default:
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		resp := ErrorResponse{
+			Code:    "400",
+			Message: "Method not allowed",
+		}
+		writeXML(w, http.StatusMethodNotAllowed, resp)
 	}
 }
 
 func (s *Server) createBucket(w http.ResponseWriter, r *http.Request, name string) {
 	if err := s.svc.CreateBucket(name); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		resp := ErrorResponse{
+			Code:    "409",
+			Message: err.Error(),
+		}
+		writeXML(w, http.StatusConflict, resp)
 		return
 	}
+	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Created bucket " + name))
+	resp := PutObjectResult{ETag: "Created bucket " + name}
+	xml.NewEncoder(w).Encode(resp)
 }
 
 func (s *Server) listBuckets(w http.ResponseWriter, r *http.Request) {
@@ -129,7 +155,7 @@ func (s *Server) uploadObject(w http.ResponseWriter, r *http.Request, bucket, ob
 	}
 	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(http.StatusOK)
-	resp := PutObjectResult{ETag: ""}
+	resp := PutObjectResult{ETag: "file uploaded successfully"}
 	xml.NewEncoder(w).Encode(resp)
 }
 
@@ -140,30 +166,39 @@ func (s *Server) getObject(w http.ResponseWriter, r *http.Request, bucket, objec
 	bytes, contentType, err = s.svc.GetObject(bucket, object)
 	if err != nil {
 		switch err.Error() {
-		case "no bucket found":
-			http.Error(w, err.Error(), http.StatusNotFound)
-		case "no object found":
-			http.Error(w, err.Error(), http.StatusNotFound)
+		case "no bucket found", "no object found":
+			resp := ErrorResponse{
+				Code:    "404",
+				Message: err.Error(),
+			}
+			writeXML(w, http.StatusNotFound, resp)
 		default:
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			resp := ErrorResponse{
+				Code:    "500",
+				Message: err.Error(),
+			}
+			writeXML(w, http.StatusInternalServerError, resp)
 		}
 	}
 	w.Header().Set("Content-Type", contentType)
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write(bytes); err != nil {
-		log.Println("failed to send file:", err)
+		resp := ErrorResponse{
+			Code:    "500",
+			Message: "failed to send file: " + err.Error(),
+		}
+		writeXML(w, http.StatusInternalServerError, resp)
 	}
 }
 
 func (s *Server) deleteObject(w http.ResponseWriter, r *http.Request, bucket, object string) {
-	err := s.svc.DeleteObject(bucket, object)
-	switch err.Error() {
-	case "no bucket found":
-		http.Error(w, err.Error(), http.StatusNotFound)
-	case "no object found":
-		http.Error(w, err.Error(), http.StatusNotFound)
-	default:
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := s.svc.DeleteObject(bucket, object); err != nil {
+		resp := ErrorResponse{
+			Code:    "404",
+			Message: err.Error(),
+		}
+		writeXML(w, http.StatusNotFound, resp)
+		return
 	}
 	w.WriteHeader(http.StatusNoContent)
 }
--- internal/server/xml.go
+++ internal/server/xml.go
@@ -1,6 +1,9 @@
 package server
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"net/http"
+)
 
 type bucketXML struct {
 	XMLName          xml.Name `xml:"Bucket"`
@@ -12,4 +15,20 @@ type bucketXML struct {
 type listBucketsResultXML struct {
 	XMLName xml.Name    `xml:"ListAllMyBucketsResult"`
 	Buckets []bucketXML `xml:"Buckets>Bucket"`
+}
+
+type ErrorResponse struct {
+	XMLName xml.Name `xml:"Error"`
+	Code    string   `xml:"Code"`
+	Message string   `xml:"Message"`
+}
+
+func writeXML(w http.ResponseWriter, status int, v interface{}) {
+	w.Header().Set("Content-Type", "application/xml; chatset=utf-8")
+	w.WriteHeader(status)
+	encoder := xml.NewEncoder(w)
+	encoder.Indent("", "  ")
+	if err := encoder.Encode(v); err != nil {
+		http.Error(w, "XML encoding error", http.StatusInternalServerError)
+	}
 }
